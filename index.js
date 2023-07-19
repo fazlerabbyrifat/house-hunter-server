790761f4b856b027320731fc06bb2f7ecfd0e945
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-07-19 07:34:52 +0600

    post request of users is added

--- index.js
+++ index.js
@@ -1,18 +1,78 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('house hunter is actively looking for a new house');
-})
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.uz4gpo0.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  const userCollection = client.db("houseDB").collection("users");
+
+  try {
+    app.post("/register", async (req, res) => {
+      const { name, email, password, role } = req.body;
+
+      try {
+        const existingUser = await userCollection.findOne({ email });
+        if (existingUser) {
+          return res.status(400).json({ message: "User already exists" });
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const newUser = {
+          name,
+          email,
+          password: hashedPassword,
+          role,
+        };
+
+        const result = userCollection.insertOne(newUser)
+        res.send(result);
+      } catch (error) {
+        console.error("Registration error", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("house hunter is actively looking for a new house");
+});
 
 app.listen(port, () => {
-    console.log(`house hunter is listening on ${port}`);
-})
+  console.log(`house hunter is listening on ${port}`);
+});
